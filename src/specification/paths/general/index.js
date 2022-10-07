ceb30f20d69aed8f37a4ac17569b8881d1f04512
gfayecorrea <gqcorrea@up.edu.ph>
2022-10-07 13:22:53 +0000

    refactor: move the code towards specification as well as refactor paths

---
+++ src/specification/paths/general/index.js
@@ -0,0 +1,26 @@
+
+export const general = {
+    '/': {
+      get: {
+        summary: 'API General Example',
+        operationId: 'general',
+        responses: {
+          200: {
+            description: 'successful response',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: {
+                      type: 'boolean'
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  };
