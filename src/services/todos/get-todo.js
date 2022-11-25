e2c8acd6b465863beef15ffe13a1007d904ed9e6
gfayecorrea <gqcorrea@up.edu.ph>
2022-11-25 04:18:04 +0000

    feat: update to add security on all paths

--- src/services/todos/get-todo.js
+++ src/services/todos/get-todo.js
@@ -1,8 +1,14 @@
 import { getDB } from '../../utils/db/index.js';
 
 export const getTodo = async (request, reply) => {
-  const { params } = request;
+  const { params, username } = request;
   const { todoId: id } = params;
+
+  // check if there is username (meaning logged in)
+  if (!username) {
+    return reply.badRequest();
+  }
+ 
   const db = await getDB();
 
   const { todos } = db;
@@ -10,6 +16,11 @@ export const getTodo = async (request, reply) => {
   if (!todos[id]) {
     return reply.notFound();
   }
+
+  if (todos[id].username !== username) {
+    return reply.forbidden('You are not the owner of the todo');
+  }
+
 
   return {
     id,
